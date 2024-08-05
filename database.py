7bf86d0a9d082fac1630a64cf5d7ee5b8b0b1d20
Prashant Banerjee <prashantbanerjee80@gmail.com>
2024-08-05 05:20:22 +0000

    Secret Secured

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine, text
+import os
 
-engine = create_engine("mysql+pymysql://u1mq1qf0qiyrbddb:MkJ4q9C297Dm1yQfrF6D@baovwjcwkyn1qe5wdcgv-mysql.services.clever-cloud.com/baovwjcwkyn1qe5wdcgv?charset=utf8mb4")
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string)
 
 def load_jobs_from_db():
 
